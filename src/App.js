ef541e0d30b3f82ce08755a7789f79a1b1f5d469
ammadbaryar <ammadbaryar@gmail.com>
2024-02-08 19:57:28 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Temp from "./Components/temp";
+// import Temp from "./components/weather/temp";
+
+const App = () => {
+  return (
+    <>
+      <Temp/>
+    </>
+  );
+};
+
+export default App;
